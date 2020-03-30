e3c83de9ef84b7be779dc17b44c5b1890ffbddf8
Biuni <info.gianlucabonifazi@gmail.com>
2020-03-30 11:14:18 +0200

    Add twitter card

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,11 @@ class Footer extends Component {
       <footer>
         Created with <FontAwesomeIcon icon={faHeart} color="#eb5569" /> by <a href="https://biuni.it">Biuni</a><br />
         <small>Virus icon by <a href="https://www.flaticon.com/">Flaticon</a></small>
+        <div className="text-center mt-2">
+          <a href="https://www.buymeacoffee.com/Biuni" target="_blank" rel="noopener noreferrer">
+            <img className="AppDonation" src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" />
+          </a>
+        </div>
         <ScrollUpButton style={{ background: '#ffcdd2', fill: '#eb5569', borderRadius: '5px' }} />
       </footer>
     )
